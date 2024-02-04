1a82d1bd75da949d7f48b628a8ebd2479dffc599
Tomas <tomasbazan5@gmail.com>
2024-02-04 12:34:17 -0300

    structure: back to previous structure

--- NotedApp/setup_back.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Setting up Backend..."
-cd ./backend/noted
-docker-compose up -d
-npm install
-
-echo "Starting Backend..."
-npm run start:dev &
--- NotedApp/setup_front.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Setting up Frontend..."
-cd ./frontend/noted
-npm install
-
-echo "Starting Frontend..."
-npm run dev
